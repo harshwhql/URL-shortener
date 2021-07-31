27b67fa016648dcaf5befa2f204d5f5844816e9b
harshwhql <69953394+harshwhql@users.noreply.github.com>
2021-07-31 11:18:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import bitly_api
 from pyshorteners import Shortener
 import requests
+BITLY_ACCESS_TOKEN = "INPUT_YOUR_ ACCESS_TOKEN_HERE"
 
 def expandd():
 	short_url =input("Enter the URL: ")
-	  
-	API_Key = "bdc878983e0877a2739b47fdc92f1cb14766e011"
-	    
+	     
 	print(requests.get(short_url).url)
 
 def shorten():
-	BITLY_ACCESS_TOKEN = "bdc878983e0877a2739b47fdc92f1cb14766e011"
-
 	b = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
 
 	response = b.shorten(input("ENter the URL: "))
